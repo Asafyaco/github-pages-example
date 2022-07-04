395ba2fd2e74466ed276d0ee2e635bbde258ab96
Asaf Yacobovich <asafyaco@gmail.com>
2022-07-04 11:48:23 +0300

    deploy my react to GitGub pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import profileImage from "./assets/‏‏תמונה מסוכות חתוכה - עותק.jpeg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My React app</h1>
+      <h2>Author: Asaf Yacobovich</h2>
+      <img src={profileImage} alt="profle-image"/>
     </div>
   );
 }
